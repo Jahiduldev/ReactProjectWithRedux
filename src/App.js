f02b04f075e6eead4742e9521fa0c4840ae2f822
Jahidul islam <kzis1987@gmail.com>
2021-03-03 14:44:00 +0600

    ReactProjectWithRedux

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import MainComponent from './components/MainComponent';
+import {BrowserRouter} from 'react-router-dom';  
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <MainComponent />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/body/About.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class About extends Component {
+
+    render(){
+
+        return (
+            <div>
+               <p>About page</p>
+            </div>
+        );
+    }
+
+}
+
+export default About;
